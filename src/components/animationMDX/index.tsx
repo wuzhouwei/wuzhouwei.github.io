ddc2bb1a6f6896a27d141cb72acc0fd8968b416b
yuxinhu2 <wzw1561628221@gmail.com>
2025-07-30 15:46:46 +0800

    pref: other

--- src/components/animationMDX/index.tsx
+++ src/components/animationMDX/index.tsx
@@ -13,28 +13,48 @@ const Index = () => {
       background: "#FFB6C1",
       fonts: [{ text: "Luck", top: "35%" }],
     },
-    { x: 1, y: 0, background: "#4169E1", fonts: [{ text: "Bad", top: "35%" }] },
+    {
+      x: 1,
+      y: 0,
+      background: "#4169E1",
+      fonts: [{ text: "Luck", top: "35%" }],
+    },
     {
       x: 2,
       y: 0,
       background: "#00FFFF",
       fonts: [{ text: "Luck", top: "35%" }],
     },
-    { x: 2, y: 1, background: "#00FF7F", fonts: [{ text: "Bad", top: "35%" }] },
+    {
+      x: 2,
+      y: 1,
+      background: "#00FF7F",
+      fonts: [{ text: "Luck", top: "35%" }],
+    },
     {
       x: 2,
       y: 2,
       background: "#FFD700",
       fonts: [{ text: "Luck", top: "35%" }],
     },
-    { x: 1, y: 2, background: "#FF7F50", fonts: [{ text: "Bad", top: "35%" }] },
+    {
+      x: 1,
+      y: 2,
+      background: "#FF7F50",
+      fonts: [{ text: "Bad", top: "35%" }],
+    },
     {
       x: 0,
       y: 2,
       background: "#FA8072",
       fonts: [{ text: "Luck", top: "35%" }],
     },
-    { x: 0, y: 1, background: "#7FFFAA", fonts: [{ text: "Bad", top: "35%" }] },
+    {
+      x: 0,
+      y: 1,
+      background: "#7FFFAA",
+      fonts: [{ text: "Luck", top: "35%" }],
+    },
   ]);
   const [buttons] = useState([
     {
